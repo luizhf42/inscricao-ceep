c98e81a080f44fe771d4cde278b8d41c2bb512cd
Luiz Henrique <87872057+luizhf42@users.noreply.github.com>
2023-09-04 11:41:43 -0300

    aaa

--- insereAluno.php
+++ insereAluno.php
@@ -1,6 +1,17 @@
 <?php
 include_once("conexao.php");
-$numero = $_POST['']
+$numero_inscricao = $_POST['numero_inscricao'];
+$nome = $_POST['nome'];
+$rg = $_POST['rg'];
+$expedido_por = $_POST['expedido_por'];
+$cpf = $_POST['cpf'];
+$data_nascimento = $_POST['data_nascimento'];
+$numero_dependentes = $_POST['numero_dependentes'];
+$endereco = $_POST['endereco'];
+$nome = $_POST['nome'];
+$nome = $_POST['nome'];
+$nome = $_POST['nome'];
+$nome = $_POST['nome'];
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 $datainicio = date("y:m:d h:i:s");
@@ -20,21 +31,31 @@ $ultimo_id = mysqli_insert_id($conn);
 echo "ID: $ultimo_id <br>";
 ?>
 
-<label>Número de inscrição: <input name="numero" type="number" class="numero-inscricao"></label>
-<label>Nome: <input name="nome" type="text"></label>
-<fieldset class="rg">
-    <label>RG: <input name="rg" type="number"></label>
-    <label>Expedido por: <input name="expedidoPor" type="text"></label>
-    <label>CPF: <input name="cpf" type="number"></label>
-    <label>Data de nascimento: <input name="nascimento" type="date"></label>
-    <label>N° de dependentes: <input name="dependentes" type="text"></label>
-    <label>Endereço: <input name="" type="text"></label>
-    <label>N°: <input name="" type="number"></label>
-</fieldset>
-<label>Município: <input name="" type="text"></label>
-<label>UF: <input name="" type="text" maxlength="2"></label>
-<label>Fone: <input name="" type="tel"></label>
-<label>Celular/WhatsApp: <input name="" type="tel"></label>
-<label>Telefone para recado: <input name="" type="tel"></label>
-<label>Nome: <input name="" type="text"></label>
-<label>E-mail: <input name="" type="email"></label>
+<div>
+    <label>Número de inscrição: <input name="numero_inscricao" type="number" class="numero-inscricao"></label>
+    <label>Nome: <input name="nome" type="text"></label>
+    <fieldset class="rg">
+        <label>RG: <input name="rg" type="number"></label>
+        <label>Expedido por: <input name="expedido_por" type="text"></label>
+        <label>CPF: <input name="cpf" type="number"></label>
+        <label>Data de nascimento: <input name="data_nascimento" type="date"></label>
+        <label>N° de dependentes: <input name="numero_dependentes" type="text"></label>
+        <label>Endereço: <input name="endereco" type="text"></label>
+        <label>N°: <input name="numero" type="number"></label>
+    </fieldset>
+    <label>Município: <input name="municipio" type="text"></label>
+    <label>UF: <input name="uf" type="text" maxlength="2"></label>
+    <label>Fone: <input name="telefone" type="tel"></label>
+    <label>Celular/WhatsApp: <input name="celular" type="tel"></label>
+    <label>Telefone para recado: <input name="telefone_recado" type="tel"></label>
+    <label>Nome: <input name="nome_recado" type="text"></label>
+    <label>E-mail: <input name="email" type="email"></label>
+</div>
+
+<div>
+    <legend>CURSO PRETENDIDO</legend>
+    <select name="curso_pretendido">
+        <option value="1">Técnico em Enfermagem - Subsequente (Noite)</option>
+        <option value="2">Técnico em Farmácia - Subsequente (Noite)</option>
+    </select>
+</div>
